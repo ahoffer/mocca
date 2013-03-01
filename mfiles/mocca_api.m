c28c0b870b2e8039a60fc89c18200c5d62b83e28
Aaron Hoffer <github@hoffer.org>
2013-02-28 16:26:49 -0800

    Started integrating PCA into routines
    Next taks is to step through MOCCA and make sure the discriminating set
    is being correctly calculated.

---
+++ mfiles/mocca_api.m
@@ -0,0 +1,44 @@
+function results=mocca_api(data, width, alpha, beta, epsilon, ...
+  subspace_overlap_threshold, ...
+  object_overlap_threshold, ...
+  use_pca,...
+  rotation_set_size,...
+  num_pcs)
+
+num_dims=columns(data);
+num_trials=ktrials(epsilon, alpha, beta, num_dims);
+discrim_set_size=max(2, s_est(num_dims, beta));
+num_objs=rows(data);
+results = [];
+
+%Validate inputs
+if rotation_set_size > num_objs
+  fprintf('Rotation set size, %d, cannot be larger the number of objects in the data set, %d.\nExiting...\n', rotation_set_size, num_objs);
+  return
+end
+
+if num_pcs >= num_dims
+  fprintf('Number of principal components, %d, cannot exceed number of dimensions, %d.\nExiting...\n', num_pcs, num_dims);
+  return
+end
+
+if num_pcs >= rotation_set_size
+  fprintf('Need at least %d objects to get %d principal compponents. Only %d objects requested.\nExiting...\n', num_pcs+1, num_pcs, rotation_set_size);
+  return
+end
+
+if discrim_set_size >= rotation_set_size
+  fprintf('Size of discriminating set, %d, cannot be >= size of rotation set, %d\nExiting...\n', discrim_set_size, rotation_set_size);
+
+%fprintf('Number of trials=%d\n', num_trials);
+
+results=mocca(data, width, alpha, beta, ...
+  subspace_overlap_threshold, ...
+  object_overlap_threshold, ...
+  num_trials, ...
+  discrim_set_size,...
+  use_pca,...
+  rotation_set_size,...
+  num_pcs);
+
+end
---
+++ mfiles/test_mocca.m
@@ -0,0 +1,92 @@
+%Test MOCCA on random data
+
+%Bring Figure window to the foreground
+shg;
+
+%Multivariate Gaussian distrubtion variances
+xx = 4;
+yy = 2;
+%xy = 2;
+
+%Iterature over the covariance of the X and Y values to provide
+%different amount of correlation (+, 0, -)
+for xy = linspace(-2, 2, 15)
+  %Reset the drawing context
+  hold off;
+   
+  %Create distribution data
+  A = mvnrnd([0 0], [xx xy; xy yy], 100);
+  
+  %Get principal components of data
+  coeff = pca(A);
+   
+  %Rotate the data to align with the first principal component
+  %PCs are column vectors, ordered in significant from left to right
+  B = A*coeff;
+  B = fliplr(B);
+  
+  %Get SEPC clsuters
+  results=mocca_api(A, 1, 0.1, 0.3, 0.01, .4, 0.1, false, 5, -1);
+  
+  %Get number of clusters and a color map for the cluster
+  num_clus= columns(results);
+  cmap = colormap(prism(num_clus));
+  fprintf('Number of clusters %d\n', num_clus);
+  
+  %Iterature over the clusters and draw each one in a different color.
+  for i=1:num_clus
+    
+    %X and Y values of each point in the cluster
+    clusX = A(results(i).objects, 1);
+    clusY = A(results(i).objects, 2);
+    
+    %Assign a color (RGB 1x3) triple to the clsuter
+    color_vec=cmap(i,:);
+    
+    %Plot the cluster
+    if results(i).num_congregating_dims == 2      
+      scatter(clusX, clusY, 80, color_vec, 'filled', 'o');      
+    else
+      scatter(clusX, clusY, 80, color_vec, '.');      
+    end
+        
+    %Format the plot
+    axis('square')
+    %grid on;
+    xlabel('X Axis');
+    ylabel('Y Axis');
+    xlim([-5, 6]);
+    ylim([-5 5]);
+    
+    %Turn on hold because we want to plot all the clusters
+    hold on
+  end
+  
+  %Plot all the dat as empty circles
+  h = scatter(A(:,1),A(:,2),'ko');
+  
+  %Do crazy stuff to plot principal components
+  temp=zeros(2,4);
+  temp(:, 2:2:end) = coeff;
+  scalefactor=10; %Make the lines longer
+  temp=scalefactor * temp;
+  X=temp(1:2:end);
+  Y=temp(2:2:end);
+  plot(X, Y, 'r:', 'LineWidth', 0.5);
+  
+  %Pause until keystroke
+  w = waitforbuttonpress;  
+  %Plot PCA rotated data.
+  plot(B(:,1), B(:,2), 'g.');
+  
+  %Pause until keystroke
+  w = waitforbuttonpress;
+  key = get(gcf,'CurrentCharacter');
+  disp(key);
+  if key == 'e'
+    closereq
+    return
+  end
+end
+
+closereq
